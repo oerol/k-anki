2cea2ab8363daf2b4ac0a205e7ba936fe977c8c6
oerol <erolxerolx@gmail.com>
2022-02-05 11:25:33 +0100

    existing cards are now being skipped

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 class CommonWords():
     common = []
+    current = []
 
     def getCommonWords(self):
         with open('common.txt', 'r') as file:
@@ -9,6 +10,16 @@ class CommonWords():
                     self.common.append(line.replace("\n", ""))
             return self.common
 
+    def getCurrentWords(self):
+        with open('anki.txt', 'r') as file:
+            page = file.readlines()
+            for line in page:
+                self.current.append(line.split("\t")[2])
+                
+            return self.current
+
+
 # commonWords = CommonWords().getCommonWords()
+# print(CommonWords().getCurrentWords())
 
 
