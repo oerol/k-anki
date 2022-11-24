89913847613518c2383cb364ae41b5604a21fc38
oerol <erolxerolx@gmail.com>
2022-11-24 19:28:01 +0100

    Added a seperate file for user settings

--- modules/details.py
+++ modules/details.py
@@ -2,10 +2,19 @@ import requests
 import json
 
 
+settings_file_path = "settings.json"
+merriam_webster_api_key = ""
+deepl_api_key = ""
+with open(settings_file_path) as json_file:
+    settings = json.load(json_file)
+    merriam_webster_api_key = settings["merriam-webster-api-key"]
+    deepl_api_key = settings["deepl-api-key"]
+
+
 def getDefinition(word):
     print(word)
     request = requests.get(
-        "https://dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=9af79d50-77cd-4a8a-8b2d-ed565d048f13")
+        f"https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={merriam_webster_api_key}")
     response = json.loads(request.text)
     try:
         definition = response[0]["shortdef"][0]
@@ -19,7 +28,7 @@ def getTranslation(word):
     result = requests.get(
         "https://api-free.deepl.com/v2/translate",
         params={
-            "auth_key": "ca38b5b0-0c93-0b09-2e1c-2b10d4fd8742:fx",
+            "auth_key": deepl_api_key,
             "target_lang": "DE",
             "text": word,
         },
